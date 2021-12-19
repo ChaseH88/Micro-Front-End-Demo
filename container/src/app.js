a1ff9ec2ca0e5bc3e0d825b6abc98c446548ebd6
“Chase <chaseh88@gmail.com>
2021-12-19 15:07:34 -0600

    Adding intial container setup

---
+++ container/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { StylesProvider } from '@material-ui/core/styles';
+
+const App = () => (
+  <div id='marketing-component'>
+    <h1>Test</h1>
+  </div>
+)
+
+export { App };
---
+++ container/src/bootstrap.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Components
+import { App } from './app';
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
